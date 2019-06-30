eba469a5c6d44903300df651675fe820c81e6610
Robin <robinsilverhav@gmail.com>
2019-06-30 11:31:40 +0200

    removed unused file, ui handler can init buffers with default options and set content of buffer

--- rplugin/python3/spotify_control/ui_handler.py
+++ rplugin/python3/spotify_control/ui_handler.py
@@ -3,8 +3,28 @@ class UIHandler():
         self.vim = vim
         self.playlist_buffer = None
 
+    def _init_buffer_options(self, vim_buffer):
+        vim_buffer.api.set_option('modifiable', False)
+        vim_buffer.api.set_option('readonly', True)
+        vim_buffer.api.set_option('bufhidden', 'hide')
+        vim_buffer.api.set_option('buftype', 'nofile')
+        vim_buffer.api.set_option('swapfile', False)
+        vim_buffer.api.set_option('buflisted', False)
+
+    def _set_buffer_content(self, vim_buffer, lines):
+        vim_buffer.api.set_option('modifiable', True)
+        vim_buffer.api.set_option('readonly', False)
+
+        self.playlist_buffer.api.set_lines(0, -1, 0, lines)
+
+        vim_buffer.api.set_option('modifiable', False)
+        vim_buffer.api.set_option('readonly', True)
+
     def init_buffers(self, playlists):
+        # setting up playlist buffer
         self.vim.command('topleft vertical 32 new')
         self.playlist_buffer = self.vim.current.buffer
-        self.playlist_buffer.api.set_lines(0, -1, 0, playlists)
-        self.vim.command('set nomodifiable')
+
+        self._init_buffer_options(self.playlist_buffer)
+        self._set_buffer_content(self.playlist_buffer, playlists)
+
